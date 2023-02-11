5da8551836fd15d9140ad672524c814fc02d6c78
volodinakarina <volodinakarina27@yandex.ru>
2023-02-11 18:55:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,221 @@
+from datetime import datetime
+
+import json
+
+from flask import Flask, request
+
+from flask_sqlalchemy import SQLAlchemy
+
+import raw_data
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(10))
+    phone = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone,
+        }
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String())
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    address = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price,
+            "customer_id": self.customer_id,
+            "executor_id": self.executor_id,
+        }
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "executor_id": self.executor_id,
+        }
+
+
+with app.app_context():
+    db.create_all()
+
+    for usr_data in raw_data.users:
+        new_usr = User(**usr_data)
+        db.session.add(new_usr)
+        db.session.commit()
+
+    for ord_data in raw_data.orders:
+        ord_data['start_date'] = datetime.strptime(ord_data['start_date'], '%m/%d/%Y').date()
+        ord_data['end_date'] = datetime.strptime(ord_data['end_date'], '%m/%d/%Y').date()
+        db.session.add(Order(**ord_data))
+        db.session.commit()
+
+    for ofr_data in raw_data.offers:
+        db.session.add(Offer(**ofr_data))
+        db.session.commit()
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        result = []
+        for user in User.query.all():
+            result.append(user.to_dict())
+
+        return json.dumps(result), 200
+
+    elif request.method == 'POST':
+        user_data = json.loads(request.data)
+        db.session.add(User(**user_data))
+        db.session.commit()
+        return '', 201, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/users/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
+def user(uid: int):
+    user = User.query.get(uid).to_dict()
+    if request.method == 'GET':
+        return json.dumps(user), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+    if request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return '', 204
+    if request.method == 'PUT':
+        user_data = json.loads(request.data)
+        user.first_name = user_data["first_name"]
+        user.last_name = user_data["last_name"]
+        user.age = user_data["age"]
+        user.email = user_data["email"]
+        user.role = user_data["role"]
+        user.phone = user_data["phone"]
+        return '', 204
+
+
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    if request.method == 'GET':
+        result = []
+        for order in Order.query.all():
+            # result.append(order.to_dict())
+            ord_dict = order.to_dict()
+            ord_dict['start_date'] = str(ord_dict['start_date'])
+            ord_dict['end_date'] = str(ord_dict['end_date'])
+            result.append(ord_dict)
+        return json.dumps(result), 200
+
+    elif request.method == 'POST':
+        order_data = json.loads(request.data)
+        db.session.add(Order(**order_data))
+        db.session.commit()
+        return '', 201, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/orders/<int:oid>', methods=['GET', 'PUT', 'DELETE'])
+def order(oid: int):
+    if request.method == 'GET':
+        order = Order.query.get(oid)
+        ord_dict = order.to_dict()
+        ord_dict['start_date'] = str(ord_dict['start_date'])
+        ord_dict['end_date'] = str(ord_dict['end_date'])
+        return json.dumps(ord_dict), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+    if request.method == 'DELETE':
+        order = Order.query.get(oid)
+        db.session.delete(order)
+        db.session.commit()
+        return '', 204
+    if request.method == 'PUT':
+        order = Order.query.get(oid)
+        order_data = json.loads(request.data)
+        order_data['start_date'] = datetime.strptime(order_data['start_date'], '%Y-%m-%d').date()
+        order_data['end_date'] = datetime.strptime(order_data['end_date'], '%Y-%m-%d').date()
+        order.name = order_data["name"]
+        order.description = order_data["description"]
+        order.address = order_data["address"]
+        order.price = order_data["price"]
+        order.customer_id = order_data["customer_id"]
+        order.executor_id = order_data["executor_id"]
+        db.session.add(order)
+        db.session.commit()
+        return '', 204
+
+
+@app.route('/offers', methods=['GET', 'POST'])
+def offers():
+    if request.method == 'GET':
+        result = []
+        for offer in Offer.query.all():
+            result.append(offer.to_dict())
+
+        return json.dumps(result), 200
+
+    elif request.method == 'POST':
+        offer_data = json.loads(request.data)
+        db.session.add(Offer(**offer_data))
+        db.session.commit()
+        return '', 201, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/offers/<int:oid>', methods=['GET', 'PUT', 'DELETE'])
+def offer(oid: int):
+    if request.method == 'GET':
+        offer = Offer.query.get(oid).to_dict()
+        return json.dumps(offer), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+    if request.method == 'DELETE':
+        offer = Offer.query.get(oid)
+        db.session.delete(offer)
+        db.session.commit()
+        return '', 204
+    if request.method == 'PUT':
+        offer = Offer.query.get(oid)
+        offer_data = json.loads(request.data)
+        offer.order_id = offer_data["order_id"]
+        offer.executor_id = offer_data["executor_id"]
+        db.session.add(offer)
+        db.session.commit()
+        return '', 204
+
+if __name__ == '__main__':
+    app.run()
